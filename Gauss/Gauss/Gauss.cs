b36f5f3a847bbbadfa545f21b6010feeafb79054
Sazonov Sergey <2001sazonov@gmail.com>
2020-03-16 23:32:54 +0300

    Добавлен класс Гаусс

--- Gauss/Gauss/Gauss.cs
+++ Gauss/Gauss/Gauss.cs
@@ -4,9 +4,18 @@ namespace Gauss
 {
     static class Gauss
     {
-        static void SubstractRow()
+        public static void SubstractRow(Matrix m1, int k)
         {
-
+            float[,] M = m1.GetMatrix(true);
+            float m = M[k, k];
+            for (int i = k+1; i < M.GetLength(0); i++)
+            {
+                float t = M[i, k] / m;
+                for (int j=k; j < M.GetLength(1); j++)
+                {
+                    M[i, j] = M[i, j] - M[k, j] * t;
+                }
+            }
         }
     }
 }
